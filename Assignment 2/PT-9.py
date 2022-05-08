28f273fd3d8a9aa3d7b45afcd976fa8c08cc900a
Manpreet Singh <manpreets@users.noreply.github.com>
2022-05-08 20:57:01 +1000

    PT 9 done

--- Assignment 2/PT-9.py
+++ Assignment 2/PT-9.py
@@ -1,9 +1,22 @@
 # Manpreet Singh | Student id - 632027 | Programming Task 8
 import tkinter as tk
 
+
 # Distinction level callback function for Submit button
 def print_greeting():
     print('Hi there,' + text_full_name.get())
+
+
+# HD level callback function to compare emails
+def email_check():
+    if text_email.get() == '':
+        print('Please enter your email address into the first field')
+    elif text_email_again.get() == '':
+        print('Please enter your email address into the second field')
+    elif text_email.get() == text_email_again.get():
+        print('You will receive an email shortly to confirm your membership')
+    else:
+        print('The email addresses entered do not match.')
 
 
 # Pass level.
@@ -25,7 +38,8 @@ button_check = tk.Button(text='Check',
                          fg='black',
                          bg='white',
                          width=15,
-                         height=10
+                         height=10,
+                         command=email_check
                          )
 
 button_submit = tk.Button(text='Submit',
@@ -36,22 +50,28 @@ button_submit = tk.Button(text='Submit',
                           command=print_greeting
                           )
 
-text_email = tk.Entry(text='Email Address',
-                      bg='yellow',
+label_email = tk.Label(text='Enter email')
+text_email = tk.Entry(bg='yellow',
                       fg='blue',
                       width=40
                       )
 
+label_email_again = tk.Label(text='Enter email address again')
+text_email_again = tk.Entry(bg='yellow',
+                            fg='blue',
+                            width=40
+                            )
 
 # Add the widgets to the window
 label_welcome.pack()
 label_full_name.pack()
 text_full_name.pack()
+label_email.pack()
+text_email.pack()
+label_email_again.pack()
+text_email_again.pack()
 button_submit.pack()
 button_check.pack()
-text_email.pack()
 
 # Wait for the input from the user
 pt9_window.mainloop()
-
-
