adcdbaa19d1e798c3a434bd04efc40e587ec107a
Manpreet Singh <manpreets@users.noreply.github.com>
2022-05-08 16:40:49 +1000

    Distinction level done

--- Assignment 2/PT-9.py
+++ Assignment 2/PT-9.py
@@ -1,56 +1,57 @@
 # Manpreet Singh | Student id - 632027 | Programming Task 8
 import tkinter as tk
+
+# Distinction level callback function for Submit button
+def print_greeting():
+    print('Hi there,' + text_full_name.get())
+
+
 # Pass level.
 pt9_window = tk.Tk()
-# Pass level main function
-def run_pass_level():
-    pt9_window.title('ZAT113 Task 9')
+pt9_window.geometry('800x600')
+
+pt9_window.title('ZAT113 Task 9')
+label_welcome = tk.Label(text='Welcome to this page. Please enter your details below')
+label_full_name = tk.Label(text='Full name : ')
+# End pass level
+
+# Start credit level
+text_full_name = tk.Entry(bg='blue',
+                          fg='yellow',
+                          width=30)
+
+# Credit and distinction levels
+button_check = tk.Button(text='Check',
+                         fg='black',
+                         bg='white',
+                         width=15,
+                         height=10
+                         )
+
+button_submit = tk.Button(text='Submit',
+                          fg='black',
+                          bg='white',
+                          width=15,
+                          height=10,
+                          command=print_greeting
+                          )
+
+text_email = tk.Entry(text='Email Address',
+                      bg='yellow',
+                      fg='blue',
+                      width=40
+                      )
 
 
-# Credit level
+# Add the widgets to the window
+label_welcome.pack()
+label_full_name.pack()
+text_full_name.pack()
+button_submit.pack()
+button_check.pack()
+text_email.pack()
 
-# Credit level main function
-def run_credit_level():
-
-
-# Distinction level
-
-# Distinction level main function
-def run_distinction_level():
-
-
-# HD level
-
-# HD level main function
-def run_hd_level():
-
-# Main function
-def main():
-    while True:
-        print('\n\n')
-        print('##########-----------Programming Task 7 - Main menu-------------##########')
-        user_selection = input('Please press 0, 1 or 2 to select an option from below : '
-                               + '\n 0 - Exit the program '
-                               + '\n 1 - Test Pass level'
-                               + '\n 2 - Test Credit level '
-                               + '\n 3 - Test Distinction '
-                               + '\n 4 - Test HD Level '
-                               )
-
-        if user_selection == '0':
-            break
-        elif user_selection == '1':
-            run_pass_level()
-        elif user_selection == '2':
-            run_credit_level()
-        elif user_selection == '3':
-            run_distinction_level()
-        elif user_selection == '4':
-            run_hd_level()
-        else:
-            print('Wrong input!')
-            continue
+# Wait for the input from the user
+pt9_window.mainloop()
 
 
-# Run main function
-main()
